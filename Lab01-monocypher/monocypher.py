deb8ae640603d1f6d2993f0ee4de8fd24cc34522
JesseStuy20 <jxie20@stuy.edu>
2021-09-27 22:25:30 -0400

    Created blank letter frequency list

--- Lab01-monocypher/monocypher.py
+++ Lab01-monocypher/monocypher.py
@@ -2,3 +2,15 @@
 #Lab 01 - Monocypher
 #Cybersecurity
 #2021-09-27
+
+f = open("alice.txt", "r")
+alice = f.read()
+alice = alice.lower()
+letterFrequencyList = []
+for i in range(26):
+    letterFrequencyList.append(0)
+print(len(alice))
+letterCount = 0 
+
+
+    
