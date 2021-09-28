8649901a53076ccff07f4efa0a4b04fdd3c91ca2
JesseStuy20 <jxie20@stuy.edu>
2021-09-27 22:42:53 -0400

    Added letter counting and letter counting, with frequency values printed out

--- Lab01-monocypher/monocypher.py
+++ Lab01-monocypher/monocypher.py
@@ -3,6 +3,8 @@
 #Cybersecurity
 #2021-09-27
 
+#1.1
+
 f = open("alice.txt", "r")
 alice = f.read()
 alice = alice.lower()
@@ -12,5 +14,23 @@ for i in range(26):
 print(len(alice))
 letterCount = 0 
 
+for char in alice:
+    if (char.isalpha()):
+        added = False
+        i = 0
+        while added is False:
+            if (ord(char)-97 == i):
+                letterFrequencyList[i] = letterFrequencyList[i] + 1
+                added = True
+            else:
+                i = i + 1
+        letterCount = letterCount + 1
+
+print(letterCount)
+
+for i in range(26):
+   letterFrequencyList[i] = letterFrequencyList[i] / letterCount
+   print(str(chr(i+97)) + ":" + str(letterFrequencyList[i]))
+ 
 
     
