d6d6af98d45413d34a6f5ec277fbdb9a1797c1a3
JesseStuy20 <jxie20@stuy.edu>
2021-09-29 22:04:38 -0400

    Used shift to output decrypted text, and tested with examples (gtji outputs ruet, but others all work fine)

--- Lab01-monocypher/monocypher.py
+++ Lab01-monocypher/monocypher.py
@@ -11,13 +11,12 @@ import math
 with open(sys.argv[1], 'r') as f:
     text = f.read()      
 
-text = text.lower()
 letterFrequencyList = []
 for i in range(26):
     letterFrequencyList.append(0.0)
 letterCount = 0
 
-for char in text:
+for char in text.lower():
     if (char.isalpha()):
         added = False
         i = 0
@@ -34,13 +33,14 @@ for i in range(26):
    if (sys.argv[2] == "frequency"):
        print(str(chr(i+97)) + ":" + str(letterFrequencyList[i]))
 
-relativeFrequency = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702,
-                     0.02228, 0.02015, 0.06094, 0.06966, 0.00153,
-                     0.00772, 0.04025, 0.02406, 0.06749, 0.07507,
-                     0.01929, 0.00095, 0.05987, 0.06327, 0.09056,
-                     0.02758, 0.00978, 0.02360, 0.00150, 0.01974,
-                     0.00074
-                     ]
+relativeFrequency = [
+    0.08167, 0.01492, 0.02782, 0.04253, 0.12702,
+    0.02228, 0.02015, 0.06094, 0.06966, 0.00153,
+    0.00772, 0.04025, 0.02406, 0.06749, 0.07507,
+    0.01929, 0.00095, 0.05987, 0.06327, 0.09056,
+    0.02758, 0.00978, 0.02360, 0.00150, 0.01974,
+    0.00074
+    ]
        
 if (sys.argv[2] == "decode"):
      closest = sys.maxsize
@@ -56,6 +56,15 @@ if (sys.argv[2] == "decode"):
          if (current < closest):
              closest = current
              shift = i
-     print(shift)
-
+     newText = ""
+     for char in text:
+         if (char.isalpha()):
+             newOrd = ord(char)-shift
+             if ((newOrd < 97) and (char.islower())):
+                 newOrd += 26
+             if ((newOrd < 65) and (char.isupper())):
+                 newOrd += 26
+             newText += str(chr(newOrd))
+         else: newText += char
+     print(newText)
     
