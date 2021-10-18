dd08c3e572b5821151a1e0813a601794616625f4
JesseStuy20 <jxie20@stuy.edu>
2021-10-18 17:25:03 -0400

    added xor method for strings

--- Lab03-XOR/xor.py
+++ Lab03-XOR/xor.py
@@ -2,3 +2,26 @@
 #Cybersecurity
 #Lab03 - XOR Cipher
 #2021-10-18
+
+
+import sys
+mode = sys.argv[1]
+keyfile = sys.argv[2]
+inpfile = sys.argv[3]
+key = open(keyfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+inp = open(inpfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+debug = False
+
+if(debug):
+  print("mode:"+mode)
+  print("key: "+key)
+  print("inp: "+inp)
+
+def xor(key,inp):
+    output = ""
+    keyIndex = 0
+    for char in inp:
+        output += (ord(char) ^ ord(key[keyIndex])) + ' '
+        keyIndex += 1
+        if keyIndex == len(key):
+            keyIndex = 0
